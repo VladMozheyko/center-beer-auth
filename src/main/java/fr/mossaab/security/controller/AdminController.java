0b8b269cc0cfefd855bbf65c3bf1744a46ccd071
Asus <aalleexxaannddrr2003@list.ru>
2025-11-10 06:50:19 +0300

    fix

--- src/main/java/fr/mossaab/security/controller/AdminController.java
+++ src/main/java/fr/mossaab/security/controller/AdminController.java
@@ -1,5 +1,6 @@
 package fr.mossaab.security.controller;
 
+import fr.mossaab.security.backup.BackupService;
 import fr.mossaab.security.dto.user.GetAllUsersResponse;
 import fr.mossaab.security.service.AdminService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,12 +18,30 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AdminController {
     private final AdminService adminService;
-
+    private final BackupService backupService;
     @Operation(summary = "Получить всех пользователей", description = "Этот endpoint возвращает список всех пользователей с пагинацией.")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/all-users")
     public ResponseEntity<GetAllUsersResponse> getAllUsers(@RequestParam(defaultValue = "0") int page,
                                                            @RequestParam(defaultValue = "10") int size) {
         return ResponseEntity.ok(adminService.getAllUsers(page, size));
+    }
+    @PostMapping("/backup-now")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<String> backupNow() {
+        backupService.save();
+        return ResponseEntity.ok("backup.json сохранён");
+    }
+    @PostMapping("/restore-now")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<String> restoreNow() {
+        try {
+            backupService.restoreIfExists();
+            return ResponseEntity.ok("Данные успешно восстановлены из backup.json");
+        } catch (Exception ex) {
+            return ResponseEntity
+                    .internalServerError()
+                    .body("Ошибка при восстановлении: " + ex.getMessage());
+        }
     }
 }
