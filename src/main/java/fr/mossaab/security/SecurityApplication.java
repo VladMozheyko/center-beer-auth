1a064269b74ce9683c12046623dba3bd8cb3556f
Dentapinos <ladchenkovden@gmail.com>
2026-07-14 14:20:28 +0300

    fix: исправил недочеты с аннотациями, что бы не появлялись предупреждения

--- src/main/java/fr/mossaab/security/SecurityApplication.java
+++ src/main/java/fr/mossaab/security/SecurityApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableCaching
 public class SecurityApplication {
 
     public static void main(String[] args) {
